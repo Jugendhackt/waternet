6aea698ae72569078160cac1fda4ef8fb4602a6b
Frehd <39314181+Frehd@users.noreply.github.com>
2022-11-05 21:53:52 +0100

    added more images and added particle recognition

--- code/water-analysis/main.py
+++ code/water-analysis/main.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 import sys
 
+import matplotlib.pyplot as plt
+
 # cam = cv.VideoCapture(0)
 # retval, frame = cam.read()
 # if retval != True:
 #    raise ValueError("Can't read frame")
 
-img = cv.imread("../../documentation/resources/test.jpg")
+img = cv.imread("../../documentation/resources/test2.jpg")
 img_bw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 den = cv.fastNlMeansDenoising(img_bw, 10, 10, 7, 21)
 cv.imwrite("out/den.png", den)
@@ -16,7 +18,19 @@ img_bin = cv.bitwise_not(img_bin)
 cv.imwrite(f"out/out_img_bin.png", img_bin)
 
 (cnt, hierarchy) = cv.findContours(
-    img_bin.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    img_bin.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)  # cv.CHAIN_APPROX_SIMPLE
+cnt = tuple(filter(lambda x: cv.contourArea(x) > 0, cnt))
+
 img_contours = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 cv.drawContours(img_contours, cnt, -1, (0, 255, 0), 1)
 cv.imwrite("out/cont.png", img_contours)
+
+areas = [cv.contourArea(cv.convexHull(x)) for x in cnt]
+
+n, bins, patches = plt.hist(areas, 50, density=True, facecolor='g', alpha=0.75)
+
+plt.xlabel('pixel count')
+plt.ylabel('Occurrence')
+plt.title(f'Histogram of speck area ({len(cnt)} specks)')
+plt.grid(True)
+plt.show()
